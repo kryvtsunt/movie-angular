d6a9b577ee445cc999854605b6ee7986c6a96677
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-06-24 16:19:52 -0400

    updated movie model

--- src/app/models/movie.model.client.ts
+++ src/app/models/movie.model.client.ts
@@ -1,7 +1,20 @@
-export class Movie{
-  _id: string
-  id: string
-  title: string
-  poster_path: string
-  overview: string
+export class Movie {
+  _id: string;
+  id: string;
+  imdb_id: string;
+  title: string;
+  poster_path: string;
+  overview: string;
+  revenue: string;
+  budget: string;
+  runtime: string;
+  vote_average: string;
+  vote_count: string;
+  status: string;
+  release_date: string;
+  homepage: string;
+  tagline: string;
+  genres: {};
+  production_countries: {};
+  production_companies: {};
 }
